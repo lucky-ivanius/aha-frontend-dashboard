00aa12c77d9248b489c2604f2edf84c57795e5ec
Lucky Ivanius <luckzivanius@gmail.com>
2025-02-28 14:04:59 +0700

    feat: use server proxy for api

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // Use manually loaded env
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
